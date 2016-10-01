26e24d253e6d84e5f0e5270adad7ced7d8d5b39c
Frank Duncan <frank@kank.net>
2016-10-01 15:15:39 -0500

    client - boot provider on configured port

--- src/main/package.lisp
+++ src/main/package.lisp
@@ -6,6 +6,6 @@
   "The router for the microservices to register with and use."))
 
 (defpackage #:microservices-client (:use :common-lisp)
- (:export)
+ (:export #:boot)
  (:documentation
   "The client library that clients use impelemnt and register their services."))
